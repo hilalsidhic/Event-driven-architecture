d1258f1d747aa27e58342737685eae70baa33dc2
hilalsidhic <hilalsidhic21@gmail.com>
2023-05-08 01:33:57 +0530

    initial commit

---
+++ api.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection,HashModel
+import consumers
+import json
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+redis = get_redis_connection(
+    host='redis-12389.c244.us-east-1-2.ec2.cloud.redislabs.com',
+    port=12389,
+    password='o2WBFJ5yPZoeDLh2AxCZOR7bKEg8gyDL',
+    decode_responses=True
+)
+
+class Delivery(HashModel):
+    budget: int = 0 
+    notes : str = ''
+
+    class Meta:
+        database = redis
+
+class Event(HashModel):
+    delivery_id: str = None 
+    type: str
+    data: str
+
+    class Meta:
+        database = redis
+
+@app.get('/deliveries/{pk}/status')
+async def status(pk: str):
+    data = redis.get(f'delivery:{pk}')
+    if data is not None:
+        return json.loads(data)
+    return {}
+
+@app.post('/deliveries/create')
+async def create(request: Request):
+    body = await request.json()
+    delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
+    event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
+    state = consumers.create_delivery(delivery, event)
+    redis.set(f'delivery:{delivery.pk}', json.dumps(state))
+    return state
+
+
